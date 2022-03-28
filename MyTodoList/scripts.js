9a3ad5af5a35b12f9a48abdcd717c7f3ac7a5178
Hebert Monteiro <monteiro.hebert@gmail.com>
2022-03-28 19:14:05 -0300

    Finalizado html e css. JS, criado as funções de criar item html, atualiza e limpar tela

---
+++ MyTodoList/scripts.js
@@ -0,0 +1,58 @@
+/* <label for="" class="item">
+                <input type="checkbox">
+                <div>teste de item 1</div>
+                <input type="button" value="X">
+            </label>
+ */
+
+let dataBase = [
+  { assignment: "Estudar JS", status: "" } /* Banco de dados local*/,
+  { assignment: "Netflix", status: "checked" },
+  { assignment: "Aula faculdade", status: "" },
+];
+
+const createItem = (text, status, index) => {
+  /* Cria um novo elemwnto HTML */
+  const item = document.createElement("label");
+  item.classList.add("item");
+  item.innerHTML = `<input type="checkbox" ${status} data-index=${index}>
+                <div>${text}</div> 
+                <input type="button" value="X" data-index=${index}>`;
+  document.getElementById("todoList").appendChild(item); // adiciona o elemento 'item no elemento e id 'todoList'
+};
+
+const clearAssignment = () => {
+  /* Executa um laço que exlcui o ulitmo filho, impedindo duplicatas*/
+  const todoList = document.getElementById("todoList");
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.lastChild);
+  }
+};
+
+const update = () => {
+  clearAssignment();
+  dataBase.forEach((item, index) =>
+    createItem(item.assignment, item.status, index)
+  );
+};
+
+const newAssignment = (event) => {
+  const tecla = event.key;
+  const text = event.target.value;
+  if (tecla === "Enter") {
+    dataBase.push({ assignment: text, status: "" });
+    update();
+    event.target.value = "";
+  }
+};
+
+const clickItem = (event) => {
+  const element = event.target;
+  console.log(element);
+};
+
+/* Observa o evento de preciona tecla */
+document.getElementById("newinput").addEventListener("keypress", newAssignment);
+document.getElementById("todoList").addEventListener("click", clickItem);
+// atualiza tela
+update();
